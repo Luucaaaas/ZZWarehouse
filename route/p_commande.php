24fef7ccb305e1b035ca7128f473de94095c0db6
Lucas <lucas.alazary@ecole-isitech.fr>
2024-03-23 19:16:29 +0100

    zzwarehouse working ✅

--- route/p_commande.php
+++ route/p_commande.php
@@ -5,27 +5,53 @@ require_once 'z_int.php';
 
 $database = new Database();
 
+$column = isset($_GET['column']) ? $_GET['column'] : 'id_commande';
+$order = isset($_GET['order']) ? $_GET['order'] : 'asc';
+
 $database->query("SELECT c.id_commande, u.nom AS nom_utilisateur, s.nom AS nom_produit, c.quantite, c.type_mouvement, c.date_commande, c.statut FROM commandes c
                   JOIN utilisateurs u ON c.id_utilisateur = u.id_utilisateur
-                  JOIN stocks s ON c.id_stock = s.id_stock");
+                  JOIN stocks s ON c.id_stock = s.id_stock
+                  ORDER BY $column $order");
 $commandes = $database->resultSet();
-
 
 if (isset($_POST['valider_commande'])) {
     $commande_id = $_POST['commande_id'];
 
-    $database->query("UPDATE commandes SET statut = 'validee' WHERE id_commande = :commande_id");
+    $database->query("SELECT stocks.quantite_disponible AS stock_disponible, commandes.quantite AS quantite_commande, commandes.type_mouvement FROM commandes JOIN stocks ON commandes.id_stock = stocks.id_stock WHERE commandes.id_commande = :commande_id");
     $database->bind(':commande_id', $commande_id);
-    $database->execute();
+    $result = $database->single();
 
-    $_SESSION['messageCommande'] = 'Le statut de la commande a été mis à jour.';
+    $stock_disponible = $result->stock_disponible;
+    $quantite_commande = $result->quantite_commande;
+    $type_mouvement = $result->type_mouvement;
+
+    if ($type_mouvement === 'Sortie' && $stock_disponible < $quantite_commande) {
+        $_SESSION['messageCommande'] = 'La quantité en stock est insuffisante pour valider la commande.';
+    } else {
+        $database->query("UPDATE commandes SET statut = 'validee' WHERE id_commande = :commande_id");
+        $database->bind(':commande_id', $commande_id);
+        $database->execute();
+
+        if ($type_mouvement === 'Sortie') {
+            $nouveau_stock = $stock_disponible - $quantite_commande;
+        } else {
+            $nouveau_stock = $stock_disponible + $quantite_commande;
+        }
+
+        $database->query("UPDATE stocks SET quantite_disponible = :nouveau_stock WHERE id_stock = (SELECT id_stock FROM commandes WHERE id_commande = :commande_id)");
+        $database->bind(':nouveau_stock', $nouveau_stock);
+        $database->bind(':commande_id', $commande_id);
+        $database->execute();
+        //valide commande
+        $_SESSION['messageCommande'] = 'Le statut de la commande a été mis à jour.';
+    }
 } elseif (isset($_POST['invalider_commande'])) {
     $commande_id = $_POST['commande_id'];
 
     $database->query("UPDATE commandes SET statut = 'invalidée' WHERE id_commande = :commande_id");
     $database->bind(':commande_id', $commande_id);
     $database->execute();
-
+    // invalide commande 
     $_SESSION['messageCommande'] = 'Le statut de la commande a été mis à jour.';
 }
 
@@ -33,7 +59,6 @@ if (isset($_SESSION['messageCommande'])) {
     $messageCommande = $_SESSION['messageCommande'];
     unset($_SESSION['messageCommande']);
 }
-
 
 ?>
 
@@ -62,16 +87,109 @@ if (isset($_SESSION['messageCommande'])) {
     
 
         <table>
-            <thead>
+        <thead class="role-column">
                 <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">Utilisateur</th>
-                    <th scope="col">Produit</th>
-                    <th scope="col">Quantite</th>
-                    <th scope="col">Mouvement</th>
-                    <th scope="col">Date</th>
-                    <th scope="col">Statut</th>
-                </tr>
+                    <th scope="col">
+                    <form method="get">
+                        <button type="submit" name="column" value="id_commande">
+                            ID
+                            <?php if ($column === 'id_commande') { ?>
+                                <?php if ($order === 'asc') { ?>
+                                    <img src="../source/img/fleche-haut.png" width="30" height="30" alt="haut">
+                                <?php } else { ?>
+                                    <img src="../source/img/fleche-bas.png" width="30" height="30" alt="bas">           
+                                <?php } ?>
+                            <?php } ?>
+                        </button>
+                    <input type="hidden" name="order" value="<?php echo ($column === 'id_commande' && $order === 'asc') ? 'desc' : 'asc'; ?>">
+                </form>
+            </th>                    
+            <th scope="col">
+                <form method="get">
+                    <button type="submit" name="column" value="nom_utilisateur">
+                        Utilisateur
+                        <?php if ($column === 'nom_utilisateur') { ?>
+                            <?php if ($order === 'asc') { ?>
+                                <img src="../source/img/fleche-haut.png" width="30" height="30" alt="haut">
+                            <?php } else { ?>
+                                <img src="../source/img/fleche-bas.png" width="30" height="30" alt="bas">           
+                            <?php } ?>
+                        <?php } ?>
+                    </button>
+                    <input type="hidden" name="order" value="<?php echo ($column === 'nom_utilisateur' && $order === 'asc') ? 'desc' : 'asc'; ?>">
+                </form>
+            </th>   
+            <th scope="col">
+    <form method="get">
+        <button type="submit" name="column" value="nom_produit">
+            Produit
+            <?php if ($column === 'nom_produit') { ?>
+                <?php if ($order === 'asc') { ?>
+                    <img src="../source/img/fleche-haut.png" width="30" height="30" alt="haut">
+                <?php } else { ?>
+                    <img src="../source/img/fleche-bas.png" width="30" height="30" alt="bas">           
+                <?php } ?>
+            <?php } ?>
+        </button>
+        <input type="hidden" name="order" value="<?php echo ($column === 'nom_produit' && $order === 'asc') ? 'desc' : 'asc'; ?>">
+    </form>
+</th>                    <th scope="col">
+    <form method="get">
+        <button type="submit" name="column" value="quantite">
+            Quantite
+            <?php if ($column === 'quantite') { ?>
+                <?php if ($order === 'asc') { ?>
+                    <img src="../source/img/fleche-haut.png" width="30" height="30" alt="haut">
+                <?php } else { ?>
+                    <img src="../source/img/fleche-bas.png" width="30" height="30" alt="bas">           
+                <?php } ?>
+            <?php } ?>
+        </button>
+        <input type="hidden" name="order" value="<?php echo ($column === 'quantite' && $order === 'asc') ? 'desc' : 'asc'; ?>">
+    </form>
+</th>                    <th scope="col">
+    <form method="get">
+        <button type="submit" name="column" value="type_mouvement">
+            Mouvement
+            <?php if ($column === 'type_mouvement') { ?>
+                <?php if ($order === 'asc') { ?>
+                    <img src="../source/img/fleche-haut.png" width="30" height="30" alt="haut">
+                <?php } else { ?>
+                    <img src="../source/img/fleche-bas.png" width="30" height="30" alt="bas">           
+                <?php } ?>
+            <?php } ?>
+        </button>
+        <input type="hidden" name="order" value="<?php echo ($column === 'type_mouvement' && $order === 'asc') ? 'desc' : 'asc'; ?>">
+    </form>
+</th>                    <th scope="col">
+    <form method="get">
+        <button type="submit" name="column" value="date_commande">
+            Date
+            <?php if ($column === 'date_commande') { ?>
+                <?php if ($order === 'asc') { ?>
+                    <img src="../source/img/fleche-haut.png" width="30" height="30" alt="haut">
+                <?php } else { ?>
+                    <img src="../source/img/fleche-bas.png" width="30" height="30" alt="bas">           
+                <?php } ?>
+            <?php } ?>
+        </button>
+        <input type="hidden" name="order" value="<?php echo ($column === 'date_commande' && $order === 'asc') ? 'desc' : 'asc'; ?>">
+    </form>
+</th>                    <th scope="col">
+    <form method="get">
+        <button type="submit" name="column" value="statut">
+            Statut
+            <?php if ($column === 'statut') { ?>
+                <?php if ($order === 'asc') { ?>
+                    <img src="../source/img/fleche-haut.png" width="30" height="30" alt="haut">
+                <?php } else { ?>
+                    <img src="../source/img/fleche-bas.png" width="30" height="30" alt="bas">           
+                <?php } ?>
+            <?php } ?>
+        </button>
+        <input type="hidden" name="order" value="<?php echo ($column === 'statut' && $order === 'asc') ? 'desc' : 'asc'; ?>">
+    </form>
+</th>                </tr>
             </thead>
             <tbody>
         <?php foreach ($commandes as $commande) : ?>
@@ -90,21 +208,26 @@ if (isset($_SESSION['messageCommande'])) {
                     ?><br><?php echo $commande->type_mouvement; ?></div></td>
                 <td data-label="Date"><?php echo $commande->date_commande; ?></td>
                 <td data-label="Statut">
-                    <?php if ($commande->statut === 'validee') {
+                    <?php if ($commande->statut === 'Validee') {
                         echo '<img src="../source/img/validee.png" width="50" height="50" alt="Statut validé">';
-                    } elseif ($commande->statut === 'invalidée') {
+                    } elseif ($commande->statut === 'Invalidée') {
                         echo '<img src="../source/img/invalidee.png" width="50" height="50" alt="Statut invalidé">';
-                    }elseif ($commande->statut === 'en attente') {
+                    } elseif ($commande->statut === 'En attente') {
                         echo '<img src="../source/img/en attente.png" width="50" height="50" alt="Statut en attente">';
-                        }?><br>
-                        <h3><?php echo $commande->statut; ?></h3     >
+                    }
+                    ?><br>
+                    <h3><?php echo $commande->statut; ?></h3>
+
+                    <?php if ($commande->statut === 'En attente') : ?>
                         <form method="POST" class="inline-form">
                             <div class="flex-container-2"><br>
                                 <input type="hidden" name="commande_id" value="<?php echo $commande->id_commande; ?>">
-                                <button type="submit" name="valider_commande" class="cancel-button" onclick="return confirm('Êtes-vous sûr de vouloir valider la commande ?')">Valider</button>
-                                <button type="submit" name="invalider_commande" class="stock-button" onclick="return confirm('Êtes-vous sûr de vouloir invalider la commande ?')">Invalider</button>
-                            </form>             
-                        </td>
+                                <button type="submit" name="valider_commande" class="stock-button" onclick="return confirm('Êtes-vous sûr de vouloir valider la commande ?')">Valider</button>
+                                <button type="submit" name="invalider_commande" class="cancel-button" onclick="return confirm('Êtes-vous sûr de vouloir invalider la commande ?')">Invalider</button>
+                            </div>
+                        </form>
+                    <?php endif; ?>
+                </td>
                     </tr>
                 <?php endforeach; ?>
             <div>
--- source/Base/zzwarehouse.sql
+++ source/Base/zzwarehouse.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : jeu. 21 mars 2024 à 16:39
+-- Généré le : sam. 23 mars 2024 à 18:40
 -- Version du serveur : 10.4.32-MariaDB
 -- Version de PHP : 8.2.12
 
@@ -34,7 +34,7 @@ CREATE TABLE `commandes` (
   `quantite` int(11) DEFAULT NULL,
   `type_mouvement` enum('Entree','Sortie') DEFAULT NULL,
   `date_commande` datetime DEFAULT current_timestamp(),
-  `statut` enum('en attente','validee','invalidée') DEFAULT 'en attente'
+  `statut` enum('En attente','Validee','Invalidée') DEFAULT 'En attente'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -42,55 +42,9 @@ CREATE TABLE `commandes` (
 --
 
 INSERT INTO `commandes` (`id_commande`, `id_utilisateur`, `id_stock`, `quantite`, `type_mouvement`, `date_commande`, `statut`) VALUES
-(1, 4, 3, 20, 'Entree', '2024-03-20 09:29:04', 'en attente'),
-(2, 5, 2, 15, 'Sortie', '2024-03-20 09:29:04', 'validee'),
-(3, 6, 1, 10, 'Sortie', '2024-03-20 09:29:04', 'en attente');
-
--- --------------------------------------------------------
-
---
--- Structure de la table `details_commande`
---
-
-CREATE TABLE `details_commande` (
-  `id_details` int(11) NOT NULL,
-  `id_stock` int(11) NOT NULL,
-  `quantite` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Déchargement des données de la table `details_commande`
---
-
-INSERT INTO `details_commande` (`id_details`, `id_stock`, `quantite`) VALUES
-(1, 1, 2),
-(2, 2, 1),
-(3, 2, 3),
-(3, 3, 3);
-
--- --------------------------------------------------------
-
---
--- Structure de la table `mouvements`
---
-
-CREATE TABLE `mouvements` (
-  `id_mouvement` int(11) NOT NULL,
-  `id_stock` int(11) DEFAULT NULL,
-  `type_mouvement` enum('Entree','Sortie') DEFAULT NULL,
-  `quantite` int(11) DEFAULT NULL,
-  `date_mouvement` datetime DEFAULT current_timestamp(),
-  `id_commande` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Déchargement des données de la table `mouvements`
---
-
-INSERT INTO `mouvements` (`id_mouvement`, `id_stock`, `type_mouvement`, `quantite`, `date_mouvement`, `id_commande`) VALUES
-(1, 1, 'Entree', 5, '2024-03-20 09:31:51', 1),
-(2, 2, 'Sortie', 2, '2024-03-20 09:31:51', 2),
-(3, 3, 'Entree', 10, '2024-03-20 09:31:51', 3);
+(55, 4, 1, 10, 'Sortie', '2024-03-23 18:12:45', 'En attente'),
+(56, 4, 30, 90, 'Sortie', '2024-03-23 18:39:39', 'En attente'),
+(57, 4, 7, 17, 'Sortie', '2024-03-23 18:39:48', 'En attente');
 
 -- --------------------------------------------------------
 
@@ -132,9 +86,9 @@ CREATE TABLE `stocks` (
 --
 
 INSERT INTO `stocks` (`id_stock`, `nom`, `description`, `quantite_disponible`, `type`) VALUES
-(1, 'Aspirine', 'Analgésique et antipyrétique', 100, 'medicament'),
+(1, 'Aspirine', 'Analgésique et antipyrétique', 2201, 'medicament'),
 (2, 'Stéthoscope', 'Instrument médical pour écouter les sons internes du corps', 10, 'materiel'),
-(3, 'Amoxicilline', 'Antibiotique', 50, 'medicament'),
+(3, 'Amoxicilline', 'Antibiotique', 10, 'medicament'),
 (4, 'Tensiomètre', 'Appareil pour mesurer la pression artérielle', 5, 'materiel'),
 (5, 'Ibuprofene', 'Anti-inflammatoire non stéroïdien', 75, 'medicament'),
 (6, 'Lampe frontale', 'Source de lumière portable pour les examens médicaux', 15, 'materiel'),
@@ -146,7 +100,7 @@ INSERT INTO `stocks` (`id_stock`, `nom`, `description`, `quantite_disponible`, `
 (12, 'Antihistaminique', 'Médicament pour le traitement des allergies', 40, 'medicament'),
 (13, 'Gants médicaux', 'Équipement de protection pour les mains', 100, 'materiel'),
 (14, 'Antiémétique', 'Médicament pour le traitement des nausées et vomissements', 25, 'medicament'),
-(15, 'Oxymètre de pouls', 'Appareil pour mesurer la saturation en oxygène dans le sang', 12, 'materiel'),
+(15, 'Oxymètre de pouls', 'Appareil pour mesurer la saturation en oxygène dans le sang', 102, 'materiel'),
 (16, 'Fluorouracile', 'Médicament utilisé dans le traitement du cancer', 15, 'medicament'),
 (17, 'Cannule nasale', 'Dispositif pour l\'administration d\'oxygène par le nez', 30, 'materiel'),
 (18, 'Antibiotique topique', 'Médicament pour le traitement des infections cutanées', 50, 'medicament'),
@@ -190,9 +144,9 @@ INSERT INTO `utilisateurs` (`id_utilisateur`, `nom`, `prenom`, `email`, `mot_de_
 (3, 'client', 'client', 'client@gmail.com', '$2y$10$aRLMc/TfeQqla4NLfYfgaeydy.GRsBt1Rmg07p.fVqbeph0/E8Oxa', 3, 0, NULL),
 (4, 'Zidane', 'Zinedine', 'zizou@gmail.com', '$2y$10$dvTVZL8MG9Zd4rMxrrAv3OBRcsvIEQAmh5m1TVGGmOzJpuQJXNZMS', 1, 0, NULL),
 (5, 'San juan', 'Coco', 'coco92@gmail.com', '$2y$10$N4u.LUvm91Wfv4A51WDkIeu4gM4ayanyALFym.q5VUzz2.ZDDLpdK', 2, 1, '2024-02-25 14:58:10'),
-(6, 'Barbet', 'Nicolas', 'nicoco@gmail.com', '$2y$10$QuNkT/3HzpJ/M5YRK/32JetKFuK6JiD28Xt4l2dvYMo763gSZlRh.', 4, 0, NULL),
-(75, 'Smith', 'Emily', 'emilysmith@gmail.fr', '$2y$10$N4u.LUvm91Wfv4A51WDkIeu4gM4ayanyALFym.q5VUzz2.ZDDLpdK', 2, 0, NULL),
-(76, 'Johnson', 'Michael', 'michaeljohnson@yahoo.com', '$2y$10$N4u.LUvm91Wfv4A51WDkIeu4gM4ayanyALFym.q5VUzz2.ZDDLpdK', 3, 0, NULL),
+(6, 'Nicolas', 'Barbet', 'n.barbet@le-fourniseur.com', '$2y$10$b/cXPWwVCik/otWCHEzw6OUDGwNEqNXmtrVH8DvFl9rgUcXn8eIGu', 4, 0, NULL),
+(7, 'Smith', 'Emily', 'emilysmith@gmail.fr', '$2y$10$N4u.LUvm91Wfv4A51WDkIeu4gM4ayanyALFym.q5VUzz2.ZDDLpdK', 2, 0, NULL),
+(76, 'Johnson', 'Michael', 'michaeljohnson@yahoo.com', '$2y$10$N4u.LUvm91Wfv4A51WDkIeu4gM4ayanyALFym.q5VUzz2.ZDDLpdK', 4, 0, NULL),
 (77, 'Davis', 'Jessica', 'jessicadavis@outlook.fr', '$2y$10$N4u.LUvm91Wfv4A51WDkIeu4gM4ayanyALFym.q5VUzz2.ZDDLpdK', 3, 0, NULL),
 (78, 'Brown', 'Christopher', 'chrisbrown@gmail.fr', '$2y$10$N4u.LUvm91Wfv4A51WDkIeu4gM4ayanyALFym.q5VUzz2.ZDDLpdK', 3, 0, NULL),
 (79, 'Wilson', 'Olivia', 'oliviawilson@yahoo.com', '$2y$10$N4u.LUvm91Wfv4A51WDkIeu4gM4ayanyALFym.q5VUzz2.ZDDLpdK', 2, 0, NULL),
@@ -237,21 +191,6 @@ ALTER TABLE `commandes`
   ADD KEY `FK_id_stock` (`id_stock`);
 
 --
--- Index pour la table `details_commande`
---
-ALTER TABLE `details_commande`
-  ADD PRIMARY KEY (`id_details`,`id_stock`),
-  ADD KEY `id_stock` (`id_stock`);
-
---
--- Index pour la table `mouvements`
---
-ALTER TABLE `mouvements`
-  ADD PRIMARY KEY (`id_mouvement`),
-  ADD KEY `id_stock` (`id_stock`),
-  ADD KEY `id_commande` (`id_commande`);
-
---
 -- Index pour la table `roles`
 --
 ALTER TABLE `roles`
@@ -279,13 +218,7 @@ ALTER TABLE `utilisateurs`
 -- AUTO_INCREMENT pour la table `commandes`
 --
 ALTER TABLE `commandes`
-  MODIFY `id_commande` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT pour la table `mouvements`
---
-ALTER TABLE `mouvements`
-  MODIFY `id_mouvement` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_commande` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=58;
 
 --
 -- AUTO_INCREMENT pour la table `roles`
@@ -297,13 +230,13 @@ ALTER TABLE `roles`
 -- AUTO_INCREMENT pour la table `stocks`
 --
 ALTER TABLE `stocks`
-  MODIFY `id_stock` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=150;
+  MODIFY `id_stock` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=153;
 
 --
 -- AUTO_INCREMENT pour la table `utilisateurs`
 --
 ALTER TABLE `utilisateurs`
-  MODIFY `id_utilisateur` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=108;
+  MODIFY `id_utilisateur` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=109;
 
 --
 -- Contraintes pour les tables déchargées
@@ -315,20 +248,6 @@ ALTER TABLE `utilisateurs`
 ALTER TABLE `commandes`
   ADD CONSTRAINT `FK_id_stock` FOREIGN KEY (`id_stock`) REFERENCES `stocks` (`id_stock`),
   ADD CONSTRAINT `commandes_ibfk_1` FOREIGN KEY (`id_utilisateur`) REFERENCES `utilisateurs` (`id_utilisateur`);
-
---
--- Contraintes pour la table `details_commande`
---
-ALTER TABLE `details_commande`
-  ADD CONSTRAINT `details_commande_ibfk_1` FOREIGN KEY (`id_details`) REFERENCES `commandes` (`id_commande`),
-  ADD CONSTRAINT `details_commande_ibfk_2` FOREIGN KEY (`id_stock`) REFERENCES `stocks` (`id_stock`);
-
---
--- Contraintes pour la table `mouvements`
---
-ALTER TABLE `mouvements`
-  ADD CONSTRAINT `mouvements_ibfk_1` FOREIGN KEY (`id_stock`) REFERENCES `stocks` (`id_stock`),
-  ADD CONSTRAINT `mouvements_ibfk_2` FOREIGN KEY (`id_commande`) REFERENCES `commandes` (`id_commande`);
 
 --
 -- Contraintes pour la table `utilisateurs`
